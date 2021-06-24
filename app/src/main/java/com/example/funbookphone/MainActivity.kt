f72bd7b38b4a04932e6dc59f77d58f9e37431738
Yurij Kuzminskij <kuzminskij5683415@gmail.com>
2021-06-24 23:55:11 +0300

    Security binding resources

--- app/src/main/java/com/example/funbookphone/MainActivity.kt
+++ app/src/main/java/com/example/funbookphone/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.funbookphone
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.funbookphone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //binding.btnBooks.setOnClickListener()
     }
     //Something new
 }
---
+++ app/src/main/java/com/example/funbookphone/security/data/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.funbookphone.security.data.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
